5282a545cd37dcb6f4dd927f1feab037dc3bba62
Norstroph <luc.alexandre.dulon@gmail.com>
2025-05-19 15:38:41 +0200

    alex

--- exo2/script2.js
+++
@@ -1,7 +0,0 @@
-
-
-const title = document.querySelector('h1')
-let change= prompt("enter your name")
-
-
-title.innerText = `Hi, I'm, ${change}`
--- script.js
+++
@@ -1,3 +0,0 @@
-const monimg = document.querySelector(".img-cat")
-monimg.src = "https://leocare.eu/fr/wp-content/uploads/2025/01/chat-bengal.jpg"
-
