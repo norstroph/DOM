fcdabde84adfd93a2c1fc3bf84b5d3a645eaaea6
Norstroph <luc.alexandre.dulon@gmail.com>
2025-05-19 14:32:04 +0200

    first

---
+++ exo2/script2.js
@@ -0,0 +1,7 @@
+
+
+const title = document.querySelector('h1')
+let change= prompt("enter your name")
+
+
+title.innerText = `Hi, I'm, ${change}`
---
+++ script.js
@@ -0,0 +1,3 @@
+const monimg = document.querySelector(".img-cat")
+monimg.src = "https://leocare.eu/fr/wp-content/uploads/2025/01/chat-bengal.jpg"
+
