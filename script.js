8371645fd901d0bb9d9a26c0ee355a28aa89f179
Norstroph <luc.alexandre.dulon@gmail.com>
2025-05-19 15:51:59 +0200

    ok

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const card = document.querySelector(".second-card")
+const image = document.createElement("img")
+ image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOXUlII4dok780-UtQX8FaS_KxRN1q4y9mOw&s"
+ card.appendChild(image)
